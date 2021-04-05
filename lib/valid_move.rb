b77c48cac03500617f7d8b60f17db4bc7f04c9e9
Jasmine Alexander <solflair.jewelry@gmail.com>
2021-04-05 17:56:45 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,25 +1,16 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?
-    return false
+  def position_taken?(board, index)
+    if board[index] == "X" || board[index] == "O"
+      return true
 
-  else
-    return true
+    elsif board[index] == " " || board[index] == ""
+      return false
 
+    elsif board[index] == nil
+      return false
+    end
 
-
-
-
-      def position_taken?(board, index)
-        if board[index] == "X" || board[index] == "O"
-          return true
-
-        elsif board[index] == " " || board[index] == ""
-          return false
-
-        elsif board[index] == nil
-          return false
-        end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
   end
